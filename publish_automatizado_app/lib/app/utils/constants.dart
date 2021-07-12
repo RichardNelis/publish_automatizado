a25c8aaabbf5e2e6f5cbd59c9f93336eb7da664b
Richard Nelis de Souza <souza.richard33@hotmail.com>
2021-07-12 11:58:21 -0300

    Update constants.dart

--- publish_automatizado_app/lib/app/utils/constants.dart
+++ publish_automatizado_app/lib/app/utils/constants.dart
@@ -2,9 +2,10 @@ const kPSistema = "/cadastro/sistema";
 const kPSistemaCN = "/cadastro/sistema_cn";
 const kPHome = '/';
 
-const kAHost = "http://10.0.2.2:5000/";
+//const kAHost = "http://10.0.2.2:5000/";
 //const kAHost = "http://127.0.0.1:5000/";
 //const kAHost = "http://localhost:5000/";
+const kAHost = "http://localhost/publish_automatizado_api/";
 const kASistema = "sistema";
 const kASistemaCN = "sistemacn";
 const kAPublish = "publish";
